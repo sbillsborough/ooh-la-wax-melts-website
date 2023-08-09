a940736243d78c50d80d2eed3de7419bb14f64a1
sbillsborough <50193655+sbillsborough@users.noreply.github.com>
2023-08-09 17:20:47 +0100

    select the parent elements by id #topnav, select all of the anchor tags with the class of btn insid eof the parent element. Loop through the anchor tags and add the active class to the current clicked tag

---
+++ assets/JS/script.js
@@ -0,0 +1,13 @@
+// Get the container element
+var navbar = document.getElementById("topnav");
+// Get all buttons with class="btn" inside the container
+var btns = navbar.getElementsByClassName("btn");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function () {
+    let current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
